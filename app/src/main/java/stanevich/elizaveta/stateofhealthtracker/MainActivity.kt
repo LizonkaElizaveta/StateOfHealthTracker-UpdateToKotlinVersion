888abcd3fbd8c681d4a22f6fc807216fbea0f800
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-07-31 16:53:20 +0300

    create toolBar menu

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/MainActivity.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/MainActivity.kt
@@ -2,6 +2,9 @@ package stanevich.elizaveta.stateofhealthtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_search) {
+            Toast.makeText(this,"Work!",Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
