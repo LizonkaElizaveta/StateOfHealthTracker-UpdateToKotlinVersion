c29436db6d6a8c8cec97a7abbc15099c9a3b5920
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-07-31 23:32:27 +0300

    add data binding

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/MainActivity.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/MainActivity.kt
@@ -7,35 +7,45 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import stanevich.elizaveta.stateofhealthtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        imageButton_excellent.setOnClickListener { toast("Work!") }
-        imageButton_satisfactorily.setOnClickListener { toast("Work!") }
-        imageButton_bad.setOnClickListener { toast("Work!") }
-        button_dyskinesia.setOnClickListener{toast("Work!")}
-        button_medication.setOnClickListener{toast("Work!")}
+        binding.apply {
+            imageButtonExcellent.setOnClickListener { toast() }
+            imageButtonSatisfactorily.setOnClickListener { toast() }
+            imageButtonBad.setOnClickListener { toast() }
+            buttonDyskinesia.setOnClickListener { toast() }
+            buttonMedication.setOnClickListener { toast() }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
+        menuInflater.inflate(R.menu.menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_search) {
-            Toast.makeText(this,"Work!",Toast.LENGTH_SHORT).show()
+        when (item.itemId) {
+            R.id.action_search -> toast()
         }
         return true
     }
 
-    private fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
-        Toast.makeText(this,message,length).show()
+
+    private fun toast(length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, R.string.toast_save_data, length).show()
     }
 }
