ff94c1901e3ba5c48dc2bdaabf87ef68dfe9d59b
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-21 20:56:20 +0300

    create navigation in all app

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/SettingsFragment.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import stanevich.elizaveta.stateofhealthtracker.R
 import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentSettingsBinding
 
@@ -14,6 +15,8 @@ class SettingsFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentSettingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        binding.set.setOnClickListener { view : View ->
+        Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_notificationsFragment)}
         return binding.root
     }
 }
--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/UsersDataFragment.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/UsersDataFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import stanevich.elizaveta.stateofhealthtracker.R
 import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentSettingsBinding
 import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentUsersDataBinding
@@ -15,6 +16,8 @@ class UsersDataFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentUsersDataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_users_data, container, false)
+        binding.users.setOnClickListener { view : View ->
+            Navigation.findNavController(view).navigate(R.id.action_settingsFragment_to_usersDataFragment)}
         return binding.root
     }
 }
