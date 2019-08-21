36efe53e9791fde3743b285e647e8a304f4ad31f
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-21 20:16:45 +0300

    create states fragment

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/StatesFragment.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/StatesFragment.kt
@@ -1,4 +1,19 @@
 package stanevich.elizaveta.stateofhealthtracker.screens.fragments
 
-class StatesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import stanevich.elizaveta.stateofhealthtracker.R
+import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentStatesBinding
+
+class StatesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentStatesBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_states, container, false)
+        return binding.root
+    }
 }
