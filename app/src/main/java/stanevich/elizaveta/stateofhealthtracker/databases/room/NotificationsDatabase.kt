2d4629426b2da5e28f6d371c1dc145d4e8e9be31
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-26 20:14:15 +0300

    create notifications database room

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/room/NotificationsDatabase.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/room/NotificationsDatabase.kt
@@ -1,4 +1,42 @@
 package stanevich.elizaveta.stateofhealthtracker.databases.room
 
-class NotificationsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import stanevich.elizaveta.stateofhealthtracker.databases.DAO.NotificationsDatabaseDao
+import stanevich.elizaveta.stateofhealthtracker.databases.entity.Notifications
+
+@Database(entities = [Notifications::class] , version = 1, exportSchema = false)
+abstract class NotificationsDatabase:RoomDatabase(){
+
+    abstract val notificationsDatabaseDao: NotificationsDatabaseDao
+
+    companion object {
+
+        /* The value of a volatile variable will never be cached, and all writes and
+         *  reads will be done to and from the main memory. */
+        @Volatile
+        private var INSTANCE: NotificationsDatabase? = null
+
+        fun getInstance(contex: Context): NotificationsDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        contex.applicationContext,
+                        NotificationsDatabase::class.java,
+                        "notifications_database_history"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
+
+    }
 }
