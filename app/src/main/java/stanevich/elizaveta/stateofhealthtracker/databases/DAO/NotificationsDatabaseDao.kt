c3db71d38139bee690f7f3f91f37e9c72d4b86fd
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-26 20:16:39 +0300

    create notifications database dao

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/DAO/NotificationsDatabaseDao.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/DAO/NotificationsDatabaseDao.kt
@@ -1,7 +1,23 @@
 package stanevich.elizaveta.stateofhealthtracker.databases.DAO
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import stanevich.elizaveta.stateofhealthtracker.databases.room.NotificationsDatabase
+import stanevich.elizaveta.stateofhealthtracker.databases.room.StatesDatabase
 
 @Dao
 interface NotificationsDatabaseDao {
+
+    @Insert
+    fun insert(notification: NotificationsDatabase)
+
+    @Query("SELECT * from notifications_table WHERE notificatiionsId =:key")
+    fun get(key:Long): NotificationsDatabase?
+
+    @Query("DELETE from notifications_table")
+    fun clear()
+
+    @Query("SELECT * from notifications_table ORDER BY notificatiionsId DESC LIMIT 1")
+    fun getLastState(): NotificationsDatabase?
 }
