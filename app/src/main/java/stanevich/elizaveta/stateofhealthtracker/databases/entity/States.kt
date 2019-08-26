4b59b89e22fedbb2475d087c0c008b1de35486ab
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-26 19:44:39 +0300

    create states entity

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/entity/States.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/entity/States.kt
@@ -1,4 +1,26 @@
 package stanevich.elizaveta.stateofhealthtracker.databases.entity
 
-class States {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "states_table")
+data class States(
+    @PrimaryKey(autoGenerate = true)
+    var statesId: Long = 0L,
+
+    @ColumnInfo(name = "date")
+    var statesDate: String = "",
+
+    @ColumnInfo(name = "time")
+    var statesTime: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "mood")
+    var statesMood: String = "",
+
+    @ColumnInfo(name = "pill")
+    var statesPill: String = "",
+
+    @ColumnInfo(name = "diskinezia")
+    var statesDiskinezia: String = ""
+)
