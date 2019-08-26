1de2373361d68b6dd1774fe747db215d51d50d92
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-26 20:07:35 +0300

    create states database DAO

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/DAO/StatesDatabaseDAO.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/DAO/StatesDatabaseDAO.kt
@@ -1,8 +1,27 @@
 package stanevich.elizaveta.stateofhealthtracker.databases.DAO
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import stanevich.elizaveta.stateofhealthtracker.databases.room.StatesDatabase
 
 @Dao
 interface StatesDatabaseDAO {
 
+    @Insert
+    fun insert(state: StatesDatabase)
+
+    @Update
+    fun update(state:StatesDatabase)
+
+    @Query("SELECT * from states_table WHERE statesId =:key")
+    fun get(key:Long): StatesDatabase?
+
+    @Query("DELETE from states_table")
+    fun clear()
+
+    @Query("SELECT * from states_table ORDER BY statesId DESC LIMIT 1")
+    fun getLastState(): StatesDatabase?
+    
 }
