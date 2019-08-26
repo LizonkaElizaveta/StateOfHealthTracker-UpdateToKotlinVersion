e56a6294fe2cecb4597529937cd1b447ef2b6f98
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-26 21:07:58 +0300

    create user data database

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/room/UsersDataDatabase.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/databases/room/UsersDataDatabase.kt
@@ -1,4 +1,42 @@
 package stanevich.elizaveta.stateofhealthtracker.databases.room
 
-class UsersDataDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import stanevich.elizaveta.stateofhealthtracker.databases.DAO.UsersDataDao
+import stanevich.elizaveta.stateofhealthtracker.databases.entity.UsersData
+
+@Database(entities = [UsersData::class], version = 1, exportSchema = false)
+abstract class UsersDataDatabase : RoomDatabase() {
+
+    abstract val statesDatabaseDao: UsersDataDao
+
+    companion object {
+
+        /* The value of a volatile variable will never be cached, and all writes and
+         *  reads will be done to and from the main memory. */
+        @Volatile
+        private var INSTANCE: UsersDataDatabase? = null
+
+        fun getInstance(contex: Context): UsersDataDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        contex.applicationContext,
+                        UsersDataDatabase::class.java,
+                        "users_database_history"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
+
+    }
 }
