935b19b66cb1fb4ee04a066f67a7a03a39799224
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-28 16:23:05 +0300

    created a view model in the StatesFragment and add it to data binding and the layout

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/factory/StatesViewModelFactory.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/factory/StatesViewModelFactory.kt
@@ -5,14 +5,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import stanevich.elizaveta.stateofhealthtracker.databases.DAO.StatesDatabaseDao
 import stanevich.elizaveta.stateofhealthtracker.screens.viewmodel.StatesViewModel
-import java.lang.IllegalArgumentException
 
-class StatesViewModelFactory(private val dataSource: StatesDatabaseDao,
-                             private val application: Application): ViewModelProvider.Factory{
+class StatesViewModelFactory(
+    private val dataSource: StatesDatabaseDao,
+    private val application: Application
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(StatesViewModel::class.java)){
-            return StatesViewModel(dataSource,application) as T
-            }
+        if (modelClass.isAssignableFrom(StatesViewModel::class.java)) {
+            return StatesViewModel(dataSource, application) as T
+        }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 
--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/StatesFragment.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/screens/fragments/StatesFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import stanevich.elizaveta.stateofhealthtracker.R
+import stanevich.elizaveta.stateofhealthtracker.databases.room.StatesDatabase
 import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentStatesBinding
+import stanevich.elizaveta.stateofhealthtracker.screens.factory.StatesViewModelFactory
+import stanevich.elizaveta.stateofhealthtracker.screens.viewmodel.StatesViewModel
 
 class StatesFragment : Fragment() {
 
@@ -16,6 +21,17 @@ class StatesFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentStatesBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_states, container, false)
         setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = StatesDatabase.getInstance(application).statesDatabaseDao
+
+        val viewModelFactory = StatesViewModelFactory(dataSource,application)
+
+        val statesViewModel = ViewModelProviders.of(this, viewModelFactory).get(StatesViewModel::class.java)
+
+        binding.lifecycleOwner = this
+
         return binding.root
     }
 
