ca1db4abac5c3b539f964e91a3e2ba2577fa8f12
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-11-03 14:20:46 +0300

    create transition between splash and tutorial

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/splash/SplashActivity.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/splash/SplashActivity.kt
@@ -1,3 +1,30 @@
 package stanevich.elizaveta.stateofhealthtracker.splash
 
-class SplashActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import stanevich.elizaveta.stateofhealthtracker.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splashViewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
+        splashViewModel.splashState.observe(this, Observer {
+            when (it) {
+                is SplashState.TutorialActivity -> {
+                    goToTutorialActivity()
+                }
+            }
+        })
+    }
+
+    private fun goToTutorialActivity() {
+        finish()
+        startActivity(Intent(this, SplashState.TutorialActivity::class.java))
+    }
+}
