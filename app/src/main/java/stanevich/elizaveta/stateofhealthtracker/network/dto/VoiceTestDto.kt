5b55c6f87c71017a114350662fb3b151396e5eec
Ilya <ilya_barbarich@mail.ru>
2020-01-07 15:08:14 +0300

    add equals and hashCode

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/network/dto/VoiceTestDto.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/network/dto/VoiceTestDto.kt
@@ -3,4 +3,22 @@ package stanevich.elizaveta.stateofhealthtracker.network.dto
 data class VoiceTestDto(
     val audioFile: String = "",
     val amp: Array<Double> = arrayOf()
-) : DataWrapper()
+) : DataWrapper() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as VoiceTestDto
+
+        if (audioFile != other.audioFile) return false
+        if (!amp.contentEquals(other.amp)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = audioFile.hashCode()
+        result = 31 * result + amp.contentHashCode()
+        return result
+    }
+}
