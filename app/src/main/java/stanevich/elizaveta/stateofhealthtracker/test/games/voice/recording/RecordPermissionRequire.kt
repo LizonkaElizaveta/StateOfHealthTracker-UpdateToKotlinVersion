25aa05b70328ab13068214d08a5a29e76b20ef41
Ilya <ilya_barbarich@mail.ru>
2020-01-10 16:55:49 +0300

    fix permission don't show again

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/voice/recording/RecordPermissionRequire.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/voice/recording/RecordPermissionRequire.kt
@@ -3,7 +3,10 @@ package stanevich.elizaveta.stateofhealthtracker.test.games.voice.recording
 import android.Manifest
 import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import stanevich.elizaveta.stateofhealthtracker.R
@@ -30,18 +33,35 @@ class RecordPermissionRequire(
                     activity.finish()
                 } else {
                     if (itShouldRequestPermissionRationaleAny()) {
-                        showDialogOK(DialogInterface.OnClickListener { dialog, which ->
-                            when (which) {
-                                DialogInterface.BUTTON_POSITIVE -> checkAndRequestPermissions()
-                                DialogInterface.BUTTON_NEGATIVE -> onRequestPermissionsResult(
-                                    requestCode,
-                                    permissions,
-                                    grantResults
-                                )
-                            }
-                        })
+                        showDialogOK(this.activity.getString(R.string.dialogText_voice_permission),
+                            DialogInterface.OnClickListener { dialog, which ->
+                                when (which) {
+                                    DialogInterface.BUTTON_POSITIVE -> checkAndRequestPermissions()
+                                    DialogInterface.BUTTON_NEGATIVE -> onRequestPermissionsResult(
+                                        requestCode,
+                                        permissions,
+                                        grantResults
+                                    )
+                                }
+                            })
                     } else {
-
+                        //ask user to go setting permission
+                        showDialogOK(this.activity.getString(R.string.dialogText_voice_permission_go_setting),
+                            DialogInterface.OnClickListener { dialog, which ->
+                                when (which) {
+                                    DialogInterface.BUTTON_POSITIVE -> {
+                                        //startActivity(Intent(this.activity, RecordPermissionsActivity::class.java))
+                                        val intent = Intent(
+                                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                            Uri.fromParts("package", activity.packageName, null)
+                                        )
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                        activity.startActivity(intent)
+                                        activity.finish()
+                                    }
+                                    DialogInterface.BUTTON_NEGATIVE -> activity.finish()
+                                }
+                            })
                     }
                 }
             }
@@ -84,9 +104,9 @@ class RecordPermissionRequire(
         }
     }
 
-    private fun showDialogOK(okListener: DialogInterface.OnClickListener) {
+    private fun showDialogOK(message: String, okListener: DialogInterface.OnClickListener) {
         AlertDialog.Builder(activity)
-            .setMessage(R.string.dialogText_voice_permission)
+            .setMessage(message)
             .setPositiveButton(R.string.btn_ok, okListener)
             .setNegativeButton(R.string.btn_cancel, okListener)
             .create()
