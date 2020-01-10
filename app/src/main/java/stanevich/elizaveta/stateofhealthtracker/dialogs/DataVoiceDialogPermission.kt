95a833eb767dde94acf29f98651646f8dfeaed6e
Ilya <ilya_barbarich@mail.ru>
2020-01-10 19:33:40 +0300

    add dialog permissions

---
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/dialogs/DataVoiceDialogPermission.kt
@@ -0,0 +1,28 @@
+package stanevich.elizaveta.stateofhealthtracker.dialogs
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import stanevich.elizaveta.stateofhealthtracker.R
+
+class DataVoiceDialogPermission(
+    private val onPositiveBtn: () -> Unit,
+    private val onNegativeBtn: () -> Unit
+) : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(context!!)
+            .setView(R.layout.dialog_voice_permission)
+            .setPositiveButton(R.string.btn_ok) { _, _ ->
+                onPositiveBtn()
+                this.dismiss()
+            }
+            .setNegativeButton(R.string.btn_cancel) { _, _ ->
+                onNegativeBtn()
+                this.dismiss()
+            }
+            .setCancelable(false)
+            .create()
+    }
+}
