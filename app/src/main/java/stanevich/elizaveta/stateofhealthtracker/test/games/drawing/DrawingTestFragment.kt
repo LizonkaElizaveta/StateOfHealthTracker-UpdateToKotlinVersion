3bf2e1f36469fbcbb215b99989c231c53514da81
LizonkaElizaveta <liza_soft_1313@mail.ru>
2020-01-29 09:58:56 +0300

    translate bitmap to base64

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/drawing/DrawingTestFragment.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/drawing/DrawingTestFragment.kt
@@ -1,10 +1,14 @@
 package stanevich.elizaveta.stateofhealthtracker.test.games.drawing
 
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
@@ -12,6 +16,7 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 import stanevich.elizaveta.stateofhealthtracker.R
 import stanevich.elizaveta.stateofhealthtracker.databinding.FragmentTestDrawingFigureBinding
 import stanevich.elizaveta.stateofhealthtracker.dialogs.TestResultDialog
+import java.io.ByteArrayOutputStream
 import java.util.*
 import kotlin.random.Random
 
@@ -30,9 +35,11 @@ class DrawingTestFragment : Fragment() {
             )
 
         val application = requireNotNull(this.activity).application
-        val figureList = listOf(R.drawable.test_drawing_figure_circle, R.drawable.test_drawing_figure_heart,
+        val figureList = listOf(
+            R.drawable.test_drawing_figure_circle, R.drawable.test_drawing_figure_heart,
             R.drawable.test_drawing_figure_pentagon, R.drawable.test_drawing_figure_square,
-            R.drawable.test_drawing_figure_star, R.drawable.test_drawing_figure_triangle)
+            R.drawable.test_drawing_figure_star, R.drawable.test_drawing_figure_triangle
+        )
 
         val figureShow =
             figureList[Random(Calendar.getInstance().timeInMillis).nextInt(figureList.size)]
@@ -51,6 +58,20 @@ class DrawingTestFragment : Fragment() {
                     NavHostFragment.findNavController(this@DrawingTestFragment)
                         .navigate(R.id.action_drawingTestFragment_to_nav_test)
                 }.show(fm, "TestResultDialog")
+
+                binding.finger.drawable?.let { drawing ->
+                    ByteArrayOutputStream().use {
+                        drawing.toBitmap()
+                            .compress(Bitmap.CompressFormat.PNG, 100, it) //bm is the bitmap object
+                        val b: ByteArray = it.toByteArray()
+                        val base64Picture = Base64.encodeToString(b, Base64.DEFAULT)
+                        Log.d("mLog", base64Picture.toString())
+                        Log.d("mLog", b.toString())
+                    }
+
+
+                }
+
             }
         }
         return binding.root
