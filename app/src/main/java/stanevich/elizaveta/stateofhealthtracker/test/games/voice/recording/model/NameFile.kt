610cceccf4d67525272e58fe290457910e38887b
Ilya Barbarich <ilya_barbarich@mail.ru>
2020-03-20 14:36:43 +0300

    fix name

--- app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/voice/recording/model/NameFile.kt
+++ app/src/main/java/stanevich/elizaveta/stateofhealthtracker/test/games/voice/recording/model/NameFile.kt
@@ -5,11 +5,12 @@ import stanevich.elizaveta.stateofhealthtracker.R
 import java.util.*
 
 object NameFile {
-    val date: Long = Calendar.getInstance().timeInMillis
+    private var date: Long = 0
 
     private val name = context?.getString(R.string.baseNameForRecordFile)
 
     fun getName(): String {
+        date = Calendar.getInstance().timeInMillis
         return "$name$date"
     }
 }
